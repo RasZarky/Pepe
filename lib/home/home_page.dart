a81ed594f142befd740e96e09e747ac271b04971
RasZarky <ubdoolrazak@gmail.com>
2024-07-09 16:47:52 +0000

    your prediction text done

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -96,7 +96,7 @@ class _HomePageState extends State<HomePage> {
                   _buildPredictionButton('SELL', Colors.red),
                 ],
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 5),
               _buildPrediction(),
               SizedBox(height: 20),
               _buildPredictionsList(),
@@ -194,26 +194,30 @@ class _HomePageState extends State<HomePage> {
     return Container(
       padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
-        color: Color(0xFF3B3A4D),
         borderRadius: BorderRadius.circular(8),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-
-          SizedBox(height: 16),
-          Text(
+          const Text(
             'Your Prediction',
-            style: TextStyle(color: Colors.white),
+            style: TextStyle(
+        fontSize: 20,
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
           ),
-          SizedBox(height: 8),
+          const SizedBox(height: 8),
           RichText(
-            text: TextSpan(
+            text: const TextSpan(
               children: [
-                TextSpan(text: 'If BTC price is ', style: TextStyle(color: Colors.white)),
-                TextSpan(text: 'above \$44,321.55 ', style: TextStyle(color: Colors.green)),
-                TextSpan(text: 'in ', style: TextStyle(color: Colors.white)),
+                TextSpan(text: 'If ', style: TextStyle(color: Color(0xFF4A4E69))),
+                TextSpan(text: 'BTC  ', style: TextStyle(color: Colors.green)),
+                TextSpan(text: 'price is ', style: TextStyle(color: Color(0xFF4A4E69))),
+                TextSpan(text: 'above \$44,321.55 \n', style: TextStyle(color: Colors.green)),
+                TextSpan(text: 'in ', style: TextStyle(color: Color(0xFF4A4E69))),
                 TextSpan(text: '60 Seconds ', style: TextStyle(color: Colors.green)),
-                TextSpan(text: 'you win ', style: TextStyle(color: Colors.white)),
+                TextSpan(text: 'you win ', style: TextStyle(color: Color(0xFF4A4E69))),
                 TextSpan(text: '\$365.00 (80%)', style: TextStyle(color: Colors.green)),
               ],
             ),
