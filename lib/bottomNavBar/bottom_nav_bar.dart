b0ac4ab7e790f0e38de799041dcd709a51c2b887
RasZarky <ubdoolrazak@gmail.com>
2024-07-10 01:32:12 +0000

    chart added

---
+++ lib/bottomNavBar/bottom_nav_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationBarWidget extends StatefulWidget {
+  @override
+  _BottomNavigationBarWidgetState createState() => _BottomNavigationBarWidgetState();
+}
+
+class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.wallet_giftcard),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.loop),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: '',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.grey,
+      backgroundColor: Color(0xFF181A20),
+      onTap: _onItemTapped,
+      type: BottomNavigationBarType.fixed,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+    );
+  }
+}
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,8 +1,12 @@
+import 'package:crypto_market/Crypto_Market/Model/coin_model.dart';
+import 'package:crypto_market/Crypto_Market/Screens/coin_candle_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:pepe/home/widgets/asset_card.dart';
 import 'package:pepe/home/widgets/header.dart';
 import 'package:pepe/home/widgets/order_card.dart';
 import 'package:pepe/home/widgets/prediction.dart';
+
+import '../bottomNavBar/bottom_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -84,7 +88,10 @@ class _HomePageState extends State<HomePage> {
 
               SizedBox(height: 20),
 
-              _buildChart(),
+              Container(
+                height: 400,
+                padding: EdgeInsets.only(top: 6),
+                  child: candleChart()),
 
               SizedBox(height: 20),
 
@@ -109,7 +116,7 @@ class _HomePageState extends State<HomePage> {
               SizedBox(height: 15),
 
               BuildPrediction(),
-              // _buildPrediction(),
+
               const Text("Predictions",
                 style: TextStyle(
                   fontSize: 20,
@@ -153,7 +160,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      bottomNavigationBar: _buildBottomNavigationBar(),
+      bottomNavigationBar: BottomNavigationBarWidget(),
     );
 }
 
@@ -172,22 +179,92 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildChart() {
-    return Container(
-      height: 200,
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Color(0xFF3B3A4D),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Center(
-        child: Text(
-          'Chart Placeholder', // Replace with your chart widget
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
+  Widget candleChart() {
+    return CandleChart(
+      coinData: coinsList.elementAt(0),
+      backgroundColor: Color(0xFF4A4E69),
+      inrRate: 77.0,
+      hideVolume: true,
+      materialInfoDialog: false,
+      intervalSelectedTextColor: Colors.red,
+      intervalTextSize: 10,
+      intervalUnselectedTextColor: Colors.black,
     );
   }
+
+  List<Coin> coinsList = [
+    Coin(
+      id: '1',
+      image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png',
+      name: 'Bitcoin',
+      shortName: 'BTC',
+      price: '123456',
+      lastPrice: '123456',
+      percentage: '-0.5',
+      symbol: 'BTCUSDT',
+      pairWith: 'USDT',
+      highDay: '567',
+      lowDay: '12',
+      decimalCurrency: 4,
+    ),
+    Coin(
+      id: '2',
+      image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png',
+      name: 'Bitcoin',
+      shortName: 'BTC',
+      price: '123456',
+      lastPrice: '123456',
+      percentage: '-0.5',
+      symbol: 'BTCINR',
+      pairWith: 'INR',
+      highDay: '567',
+      lowDay: '12',
+      decimalCurrency: 4,
+    ),
+    Coin(
+      id: '3',
+      image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
+      name: 'Binance',
+      shortName: 'BNB',
+      price: '0.0005',
+      lastPrice: '0.0005',
+      percentage: '-0.5',
+      symbol: 'BNBBUSD',
+      pairWith: 'BUSD',
+      highDay: '567',
+      lowDay: '12',
+      decimalCurrency: 4,
+    ),
+    Coin(
+      id: '4',
+      image: 'https://bin.bnbstatic.com/image/admin_mgs_image_upload/20201110/22ef2baf-b210-4882-afd9-1317bb7a3603.png',
+      name: 'Dogecoin',
+      shortName: 'DOGE',
+      price: '123456',
+      lastPrice: '123456',
+      percentage: '-0.5',
+      symbol: 'DOGEUSDT',
+      pairWith: 'USDT',
+      highDay: '567',
+      lowDay: '12',
+      decimalCurrency: 4,
+    ),
+    Coin(
+      id: '5',
+      image: 'https://bin.bnbstatic.com/image/admin_mgs_image_upload/20201110/4766a9cc-8545-4c2b-bfa4-cad2be91c135.png',
+      name: 'XRP',
+      shortName: 'XRP',
+      price: '123456',
+      lastPrice: '123456',
+      percentage: '-0.5',
+      symbol: 'XRPUSDT',
+      pairWith: 'USDT',
+      highDay: '567',
+      lowDay: '12',
+      decimalCurrency: 4,
+    ),
+  ];
+
 
   Widget _buildTimeAmountButton(String label) {
     return Expanded(
